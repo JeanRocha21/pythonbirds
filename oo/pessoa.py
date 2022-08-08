ee2e551478f99d068cbc1e287e3d5c46998e4a20
Jean Carlos Rocha <101328878+JeanRocha21@users.noreply.github.com>
2022-08-08 20:46:31 -0300

    Criado metodo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=29):
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)
     def cumprimentar(self):
         return 'Ola'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+    @classmethod
+    def nome_e_atributo_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     Jean = Pessoa(nome='Jean')
@@ -14,5 +22,9 @@ if __name__ == '__main__':
     print(Pedro.idade)
     for filho in Pedro.filhos:
         print(filho.nome)
+        print(Jean.__dict__)
+        print(Pedro.__dict__)
 
 
+    print(Pessoa.metodo_estatico(), Jean.metodo_estatico())
+    print(Pessoa.nome_e_atributo_de_classe(),  Jean.nome_e_atributo_de_classe())
